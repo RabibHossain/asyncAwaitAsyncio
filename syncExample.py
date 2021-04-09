a4191582ebc975f361e6f41c980ccfb71e41d9c6
S.M. Rabib Hossain Galib <galib.rabib@gmail.com>
2021-04-10 02:41:18 +0600

    Update syncExample.py

--- syncExample.py
+++ syncExample.py
@@ -1,5 +1,4 @@
 import time
-import asyncio
 
 def is_prime(x):
     return not any(x//i == x/i for i in range(x-1, 1, -1))
@@ -14,14 +13,10 @@ def hight_prime_below(x):
     return None
 
 def main():
-    t0 = time.time()
-    # Instead of waiting each of these functions in turn synchronously, async.io waits & takes the series of coroutines.
-    # These coroutines or asynchronous functions get called & executed. And waits until these're all done
-    
+    t0 = time.time()    
     hight_prime_below(100000)
     hight_prime_below(10000)
     hight_prime_below(1000)
-
     print('Executed time %.2f ms' % ((time.time() - t0) * 1000))
 
 main()
